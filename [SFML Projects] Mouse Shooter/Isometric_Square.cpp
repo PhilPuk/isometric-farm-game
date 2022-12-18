0463ec2333cd63f60d1bc3cec5e7811f812f0ba7
Phil_Puk <1phil.pukropski@gmx.de>
2022-12-18 15:47:40 +0100

    Correct form achieved! Find out scaling factors and to automatic calc formula for it! Added textures for the sides.

--- [SFML Projects] Mouse Shooter/Isometric_Square.cpp
+++ [SFML Projects] Mouse Shooter/Isometric_Square.cpp
@@ -13,22 +13,22 @@ void Isometric_Square::initSprite(std::vector<sf::Texture*>& texture, sf::Vector
 		this->side_visible[i] = true;
 		this->s_Square[i].setScale(scale);
 		this->s_Square[i].setPosition(pos);
+		this->s_Square[i].setTexture(*texture[i]);
 	}
 
 	this->s_Square[1].setPosition(pos.x, pos.y - 200.f);
 
-	this->s_Square[0].setTexture(*texture[0]);
+
 	this->s_Square[0].setRotation(45.f);
 	this->s_Square[0].setPosition(s_Square[0].getPosition().x, s_Square[0].getPosition().y * 2);
 
-	this->s_Square[1].setTexture(*texture[0]);
 	this->s_Square[1].setColor(sf::Color(50, 50, 255, 255));
 	this->s_Square[1].setRotation(45.f);
 	this->s_Square[1].setPosition(s_Square[1].getPosition().x, s_Square[1].getPosition().y * 2);
 
-	this->s_Square[2].setTexture(*texture[1]);
-	this->s_Square[2].setScale(scale.x * 0.7f, scale.y * 0.7f);
-	this->s_Square[2].setColor(sf::Color(200, 100, 300, 255));
+	//this->s_Square[2].setScale(scale.x * 0.7f, scale.y * 0.7f);
+	this->s_Square[2].setPosition(sf::Vector2f(this->s_Square[2].getPosition().x - this->s_Square[2].getGlobalBounds().width, this->s_Square[2].getPosition().y - this->s_Square[2].getGlobalBounds().height * 0.17f));
+	this->s_Square[3].setPosition(sf::Vector2f(this->s_Square[3].getPosition().x, this->s_Square[3].getPosition().y - this->s_Square[3].getGlobalBounds().height * 0.17f));
 }
 
 Isometric_Square::Isometric_Square(std::vector<sf::Texture*>& textures, sf::Vector2f spawn_position, sf::Vector2f scaleOfSprite)
@@ -95,6 +95,8 @@ void Isometric_Square::renderSides(sf::RenderTarget& target)
 {
 	if(this->side_visible[2])
 		target.draw(this->s_Square[2]);
+	if (this->side_visible[3])
+		target.draw(this->s_Square[3]);
 }
 
 void Isometric_Square::render(sf::RenderTarget& target)
